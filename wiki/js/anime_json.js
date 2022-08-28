0cc45eedba69d89e2d01dafb91afbfc43856cdb0
muccorz <muccorz@outlook.com>
2022-08-29 01:43:41 +0900

    到第一个或最后一个是否让上一页或下一页消失

--- wiki/js/anime_json.js
+++ wiki/js/anime_json.js
@@ -39,7 +39,7 @@ $(function() {
                 PageNum: 8, //每页显示数目
                 pageMax: true, //按钮长度是否显示
                 pageMaxHideShow: false, //在最后一个的时候是否隐藏按钮长度
-                pageDownUpHide: false, //到第一个或最后一个是否让上一页或下一页消失
+                pageDownUpHide: true, //到第一个或最后一个是否让上一页或下一页消失
                 pageInput: true, //是否使用文本框输入跳转
                 pagingBtnHide: false, //是否让按钮变为一个
                 pagingBtnPaging: true, //按钮是否分页
